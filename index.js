31e074f857efbede436f772a718b5aa2d559a89d
alejandriadavila <149810746+alejandriadavila@users.noreply.github.com>
2023-11-21 15:16:18 -0500

    Simple Math Problems Finished

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ const isSum50 = (n1 + n2 + n3 + n4) == 50
 const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2
 
 // Check three: no number larger than 25
-// This time, we use the OR operator to check
-// if ANY of the numbers is larger than 25.
-const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25
+// Changed to check if all numbers are lower than  or equal to 25
+// Changed name to if under 25
+const isUnder25 = n1 <= 25 || n2 <= 25 || n3 <= 25 || n4 <= 25
 
 // Check four: all unique numbers
 // This is long, and there are more efficient
@@ -30,25 +30,25 @@ const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 
 // for convenience. Note how we negate isOver25 using
 // the ! operator. We could also have tested for 
 // "isUnder25" as an alternative.
-const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique
+const isValid = isSum50 && isTwoOdd && isUnder25 && isUnique
 
 // Finally, log the results.
-console.log(isValid)
+console.log(`The numbers add up to 50, at least two are odd, none are larger than 25, and each number is unique: ${isValid}.`)
 
 //Check if all numbers are divisible by 5
 const isDivisibleBy5 = (n1 % 5) + (n2 % 5) + (n3 % 5) + (n4 % 5) == 0
 
 //log if divisible by 5
-console.log(isDivisibleBy5)
+console.log(`All numbers are divisible by five: ${isDivisibleBy5}`)
 
 //Check if 1st n is bigger than last n
 const isFirstLargerThanLast = n1 > n4
 
 //log if larger or not
-console.log(isFirstLargerThanLast)
+console.log(`The first number is larger than the last: ${isFirstLargerThanLast}`)
 
 //aritmentic chain problem
 const arithmeticChain = (n2-n1) * n3 % n4
 
 //log it
-console.log(arithmeticChain)
+console.log(`(${n1} - ${n2}) * ${n3} % ${n4} = ${arithmeticChain}`)
